bluestacks/cts-a16

CtsSystemApiAnnotationTestCases: Allow expected failures to be ignored
Adds support to allow a set of expected failures to be provided via the
DynamicConfig mechanism and use that in the test to filter out expected
failures.

This change includes the following:
* Adding support for DynamicConfig to the module, AndroidTest.xml,
  Android.mk changes and added DynamicConfig.xml file. The file will
  be automatically picked up by when building the test suite.
* Added ExpectedFailuresFilter and test. The test required a little
  refactoring of the existing test.
* Integrated ExpectedFailuresFilter into the AnnotationTest.java,
  populating it with information retrieved from the DynamicConfig.xml
  file.
* Improving consistency of FailureType. Added EXTRA_CONSTRUCTOR, use
  EXTRA_INTERFACE to match how MISSING_INTERFACE is used.

Usually, the above would be done as separate changes to simplify
review but having a single change makes it easier for it to be
cherry picked by partners.

Bug: 206650495
Test: atest --host signature-host-tests
Change-Id: I8657c38d386e08f8d4b969a2e30ab10b2fa879bd

tests/signature/tests/src/android/signature/cts/tests/ExpectedFailuresFilterAnnotationCheckerTest.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.signature.cts.tests;
+
+import android.signature.cts.AnnotationChecker;
+import android.signature.cts.ClassProvider;
+import android.signature.cts.ExpectedFailuresFilter;
+import android.signature.cts.FailureType;
+import android.signature.cts.JDiffClassDescription;
+import android.signature.cts.ResultObserver;
+import android.signature.cts.tests.data.ApiAnnotation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Test class for {@link ExpectedFailuresFilter} when run with {@link AnnotationChecker}.
+ */
+@RunWith(JUnit4.class)
+public class ExpectedFailuresFilterAnnotationCheckerTest
+    extends AbstractApiCheckerTest<AnnotationChecker> {
+
+    @Override
+    protected AnnotationChecker createChecker(ResultObserver resultObserver,
+            ClassProvider provider) {
+        return new AnnotationChecker(resultObserver, provider, ApiAnnotation.class.getName());
+    }
+
+    @Test
+    public void testIgnoreExpectedFailures_TestPasses() {
+        NoFailures observer = new NoFailures();
+
+        ResultObserver filter = new ExpectedFailuresFilter(observer, Arrays.asList(
+            "extra_class:android.signature.cts.tests.data.ForciblyPublicizedPrivateClass",
+            "extra_constructor:public android.signature.cts.tests.data.SystemApiClass()",
+            "extra_method:public void android.signature.cts.tests.data.SystemApiClass.apiMethod()",
+            "extra_field:public boolean android.signature.cts.tests.data.SystemApiClass.apiField"
+        ));
+
+        // Define the API that is expected to be provided by the SystemApiClass. Omitted members
+        // are actually provided by the SytstemApiClass definition and so will result in an
+        // extra_... error.
+        JDiffClassDescription clz = createClass("SystemApiClass");
+        // (omitted) addConstructor(clz);
+        // (omitted) addPublicVoidMethod(clz, "apiMethod");
+        // (omitted) addPublicBooleanField(clz, "apiField");
+
+        checkSignatureCompliance(clz, filter,
+                "android.signature.cts.tests.data.PublicApiClass");
+        // Note that ForciblyPublicizedPrivateClass is now included in the runtime classes
+    }
+
+    @Test
+    public void testIgnoreExpectedFailures_TestStillFails() {
+        NoFailures observer = new NoFailures();
+
+        ResultObserver filter = new ExpectedFailuresFilter(observer, Arrays.asList(
+            "extra_method:public void android.signature.cts.tests.data.SystemApiClass.apiMethod()",
+            "extra_field:public boolean android.signature.cts.tests.data.SystemApiClass.apiField"
+        ));
+
+        // Define the API that is expected to be provided by the SystemApiClass. Omitted members
+        // are actually provided by the SytstemApiClass definition and so will result in an
+        // extra_... error.
+        JDiffClassDescription clz = createClass("SystemApiClass");
+        addConstructor(clz);
+        // (omitted) addPublicVoidMethod(clz, "apiMethod");
+        // (omitted) addPublicBooleanField(clz, "apiField");
+
+        checkSignatureCompliance(clz, filter,
+            "android.signature.cts.tests.data.PublicApiClass",
+            "android.signature.cts.tests.data.ForciblyPublicizedPrivateClass");
+    }
+}

wwaaafa/android_cts

Merged-In: I8657c38d386e08f8d4b969a2e30ab10b2fa879bd
(cherry picked from commit 0eab8ff9344917a4cd584ae8f608a56b844cd896)

tests/signature/tests/src/android/signature/cts/tests/ExpectedFailuresFilterAnnotationCheckerTest.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.signature.cts.tests;
+
+import android.signature.cts.AnnotationChecker;
+import android.signature.cts.ClassProvider;
+import android.signature.cts.ExpectedFailuresFilter;
+import android.signature.cts.FailureType;
+import android.signature.cts.JDiffClassDescription;
+import android.signature.cts.ResultObserver;
+import android.signature.cts.tests.data.ApiAnnotation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Test class for {@link ExpectedFailuresFilter} when run with {@link AnnotationChecker}.
+ */
+@RunWith(JUnit4.class)
+public class ExpectedFailuresFilterAnnotationCheckerTest
+    extends ApiPresenceCheckerTest<AnnotationChecker> {
+
+    @Override
+    protected AnnotationChecker createChecker(ResultObserver resultObserver,
+            ClassProvider provider) {
+        return new AnnotationChecker(resultObserver, provider, ApiAnnotation.class.getName(), null);
+    }
+
+    @Test
+    public void testIgnoreExpectedFailures_TestPasses() {
+        NoFailures observer = new NoFailures();
+
+        ResultObserver filter = new ExpectedFailuresFilter(observer, Arrays.asList(
+            "extra_class:android.signature.cts.tests.data.ForciblyPublicizedPrivateClass",
+            "extra_constructor:public android.signature.cts.tests.data.SystemApiClass()",
+            "extra_method:public void android.signature.cts.tests.data.SystemApiClass.apiMethod()",
+            "extra_field:public boolean android.signature.cts.tests.data.SystemApiClass.apiField"
+        ));
+
+        // Define the API that is expected to be provided by the SystemApiClass. Omitted members
+        // are actually provided by the SytstemApiClass definition and so will result in an
+        // extra_... error.
+        JDiffClassDescription clz = createClass("SystemApiClass");
+        // (omitted) addConstructor(clz);
+        // (omitted) addPublicVoidMethod(clz, "apiMethod");
+        // (omitted) addPublicBooleanField(clz, "apiField");
+
+        checkSignatureCompliance(clz, filter,
+                "android.signature.cts.tests.data.PublicApiClass");
+        // Note that ForciblyPublicizedPrivateClass is now included in the runtime classes
+    }
+
+    @Test
+    public void testIgnoreExpectedFailures_TestStillFails() {
+        ExpectFailure observer = new ExpectFailure(FailureType.MISSING_ANNOTATION);
+
+        ResultObserver filter = new ExpectedFailuresFilter(observer, Arrays.asList(
+            "extra_method:public void android.signature.cts.tests.data.SystemApiClass.apiMethod()",
+            "extra_field:public boolean android.signature.cts.tests.data.SystemApiClass.apiField"
+        ));
+
+        // Define the API that is expected to be provided by the SystemApiClass. Omitted members
+        // are actually provided by the SytstemApiClass definition and so will result in an
+        // extra_... error.
+        JDiffClassDescription clz = createClass("SystemApiClass");
+        addConstructor(clz);
+        // (omitted) addPublicVoidMethod(clz, "apiMethod");
+        // (omitted) addPublicBooleanField(clz, "apiField");
+
+        checkSignatureCompliance(clz, filter,
+            "android.signature.cts.tests.data.PublicApiClass",
+            "android.signature.cts.tests.data.ForciblyPublicizedPrivateClass");
+    }
+}