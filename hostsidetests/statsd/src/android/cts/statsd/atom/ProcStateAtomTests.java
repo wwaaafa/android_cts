44c3b00342e8745f5f2d2502b50cc996e5b88a00
seonggoo.kang <seonggoo.kang@lge.com>
2021-12-16 15:59:50 +0900

    DO NOT MERGE: Disable the topSleeping proc state CTS on automotive
    input keyevent KEYCODE_SLEEP doesn't turn screen off on automotive.
    
    Bug: 209637686
    Test: cts-tradefed run cts -m CtsStatsdHostTestCases
    Change-Id: Ieb868b5d5526f206b3c53f9fc4d45421717519c0

--- hostsidetests/statsd/src/android/cts/statsd/atom/ProcStateAtomTests.java
+++ hostsidetests/statsd/src/android/cts/statsd/atom/ProcStateAtomTests.java
@@ -42,6 +42,7 @@ public class ProcStateAtomTests extends ProcStateTestCase {
     private static final int STATSD_REPORT_WAIT_TIME_MS = 500; // make sure statsd finishes log.
 
     private static final String FEATURE_WATCH = "android.hardware.type.watch";
+    private static final String FEATURE_AUTOMOTIVE = "android.hardware.type.automotive";
 
     // The tests here are using the BatteryStats definition of 'background'.
     private static final Set<Integer> BG_STATES = new HashSet<>(
@@ -183,6 +184,8 @@ public class ProcStateAtomTests extends ProcStateTestCase {
             return;
         }
         if (!hasFeature(FEATURE_WATCH, false)) return;
+        // Disable the topSleeping proc state CTS on automotive.
+        if (!hasFeature(FEATURE_AUTOMOTIVE, false)) return;
         Set<Integer> onStates = new HashSet<>(Arrays.asList(
                 ProcessStateEnum.PROCESS_STATE_TOP_SLEEPING_VALUE));
         Set<Integer> offStates = complement(onStates);
