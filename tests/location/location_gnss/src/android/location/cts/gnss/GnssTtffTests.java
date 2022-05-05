fdc042eb476aa683444af2a1a2e158d86f974a1e
Grace Cheng <gracemc@google.com>
2022-05-05 01:24:43 +0000

    Skips testTtffWithNetwork for automotive
    Bug: 230921567
    Test: atest CtsLocationGnssTestCases
    
    Change-Id: If75de59f0fcebb31f43e81ab9d981af71a8265aa

--- tests/location/location_gnss/src/android/location/cts/gnss/GnssTtffTests.java
+++ tests/location/location_gnss/src/android/location/cts/gnss/GnssTtffTests.java
@@ -4,6 +4,7 @@ import android.location.cts.common.GnssTestCase;
 import android.location.cts.common.SoftAssert;
 import android.location.cts.common.TestLocationListener;
 import android.location.cts.common.TestLocationManager;
+import android.location.cts.common.TestMeasurementUtil;
 import android.location.cts.common.TestUtils;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -51,6 +52,11 @@ public class GnssTtffTests extends GnssTestCase {
   @AppModeFull(reason = "permission ACCESS_LOCATION_EXTRA_COMMANDS not available to instant apps")
   public void testTtffWithNetwork() throws Exception {
     if (!TestUtils.deviceHasGpsFeature(getContext())) {
+      return;
+    }
+
+    // Network connection isn't required for automotive devices.
+    if (TestMeasurementUtil.isAutomotiveDevice(getContext())) {
       return;
     }
 
