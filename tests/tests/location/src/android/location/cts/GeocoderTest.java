6855c04f9408752d5a9e0d2c27d435c5bd128d7e
Yu-Han Yang <yuhany@google.com>
2022-07-26 20:24:00 +0000

    DO NOT MERGE: Ignore testGetFromLocationName for InstantApp
    Bug: 263707427
    Bug: 238162084, 236672058, 236447692
    Test: atest GeocoderTest
    Change-Id: I47f3b1dbf05a1c0ee24bb1bc37e6246402833ae2
    (cherry picked from commit 133edd4ad147d99840c1e87510e66ba4db5afe6f)

--- tests/tests/location/src/android/location/cts/GeocoderTest.java
+++ tests/tests/location/src/android/location/cts/GeocoderTest.java
@@ -116,6 +116,7 @@ public class GeocoderTest extends AndroidTestCase {
         }
     }
 
+    @AppModeFull(reason = "b/238831704 - Test cases don't apply for Instant apps")
     public void testGetFromLocationName() throws IOException, InterruptedException {
         Geocoder geocoder = new Geocoder(getContext(), Locale.US);
 
