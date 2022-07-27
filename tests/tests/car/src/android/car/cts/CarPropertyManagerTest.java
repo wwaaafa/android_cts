8a984f0900f44357f4f1a9d2b23aa79998d30f7c
Tyler Trephan <tylertrephan@google.com>
2022-07-27 00:16:57 +0000

    Removed @Ignore from the TIRE_PRESSURE test
    Test: atest CarPropertyManagerTest
    Fix: 238317257
    Change-Id: I44a982eadaa436bf5eb0c49e489a4e848416d4bc

--- tests/tests/car/src/android/car/cts/CarPropertyManagerTest.java
+++ tests/tests/car/src/android/car/cts/CarPropertyManagerTest.java
@@ -57,7 +57,6 @@ import com.google.common.collect.ImmutableSet;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -704,7 +703,6 @@ public class CarPropertyManagerTest extends CarApiTestBase {
                 });
     }
 
-    @Ignore("b/238317257")
     @Test
     public void testTirePressureIfSupported() {
         adoptSystemLevelPermission(/*Car.PERMISSION_TIRES=*/
