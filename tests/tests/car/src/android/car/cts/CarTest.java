29602eace92dbc32e03ec55ee4863a99523fdf34
Felipe Leme <felipeal@google.com>
2022-08-15 10:12:00 -0700

    Initial usage of ApiCheckerRule
    Test: atest android.car.cts.CarTest \
                android.car.cts.CarVersionTest \
                android.car.cts.PlatformVersionTest
    
    Bug: 242315785
    Bug: 242350638
    
    Merged-In: I19cab96231bd60bad8e3a79eed4a13b317b67df6
    Change-Id: I19cab96231bd60bad8e3a79eed4a13b317b67df6
    (cherry picked from commit 185c7e30cbf3f6e89c65c8def3590a32fc4d4a9a)

--- tests/tests/car/src/android/car/cts/CarTest.java
+++ tests/tests/car/src/android/car/cts/CarTest.java
@@ -22,6 +22,7 @@ import static com.google.common.truth.Truth.assertWithMessage;
 import android.car.Car;
 import android.car.CarVersion;
 import android.car.PlatformVersion;
+import android.car.test.ApiCheckerRule;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.ServiceConnection;
@@ -43,6 +44,7 @@ import com.android.compatibility.common.util.RequiredFeatureRule;
 
 import org.junit.After;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -58,6 +60,12 @@ public class CarTest {
     public static final RequiredFeatureRule sRequiredFeatureRule = new RequiredFeatureRule(
             PackageManager.FEATURE_AUTOMOTIVE);
 
+    @Rule
+    public final ApiCheckerRule mApiCheckerRule = new ApiCheckerRule.Builder()
+            // TODO(b/242315785): remove disable... when rule supports @AddedInOrBefore
+            .disableAnnotationsCheck()
+            .build();
+
     private static final long DEFAULT_WAIT_TIMEOUT_MS = 2000;
 
     private Context mContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
@@ -72,6 +80,9 @@ public class CarTest {
         }
     }
 
+    @ApiTest(apis = {
+            "android.car.Car#isConnected", "android.car.Car#isConnecting", "android.car.Car#connect"
+    })
     @Test
     public void testConnection() throws Exception {
         mServiceConnectionListener = new DefaultServiceConnectionListener();
@@ -87,24 +98,28 @@ public class CarTest {
         assertThat(mCar.isConnecting()).isFalse();
     }
 
+    @ApiTest(apis = {"android.car.Car#createCar(Context)"})
     @Test
     public void testBlockingCreateCar() throws Exception {
         mCar = Car.createCar(mContext);
         assertConnectedCar(mCar);
     }
 
+    @ApiTest(apis = {"android.car.Car#getCarConnectionType"})
     @Test
     public void testConnectionType() throws Exception {
         createCarAndRunOnReady((car) -> assertThat(car.getCarConnectionType()).isEqualTo(
                 Car.CONNECTION_TYPE_EMBEDDED));
     }
 
+    @ApiTest(apis = {"android.car.Car#isFeatureEnabled(String)"})
     @Test
     public void testIsFeatureEnabled() throws Exception {
         createCarAndRunOnReady(
                 (car) -> assertThat(car.isFeatureEnabled(Car.AUDIO_SERVICE)).isTrue());
     }
 
+    @ApiTest(apis = {"android.car.Car#createCar(Context,Handler,long,CarServiceLifecycleListener)"})
     @Test
     public void testCreateCarWaitForever() throws Exception {
         CarServiceLifecycleListenerImpl listenerImpl = new CarServiceLifecycleListenerImpl(null);
@@ -114,6 +129,7 @@ public class CarTest {
         listenerImpl.waitForReady(DEFAULT_WAIT_TIMEOUT_MS);
     }
 
+    @ApiTest(apis = {"android.car.Car#createCar(Context,Handler,long,CarServiceLifecycleListener)"})
     @Test
     public void testCreateCarNoWait() throws Exception {
         CarServiceLifecycleListenerImpl listenerImpl = new CarServiceLifecycleListenerImpl(null);
@@ -126,6 +142,8 @@ public class CarTest {
     }
 
     @Test
+    @ApiTest(apis = {"android.car.Car#isApiVersionAtLeast(int)",
+            "android.car.Car#isApiAndPlatformVersionAtLeast(int,int)"})
     public void testApiVersion() throws Exception {
         int ApiVersionTooHigh = 1000000;
         int MinorApiVersionTooHigh = 1000000;
@@ -157,7 +175,7 @@ public class CarTest {
 
         assertWithMessage("Car.getCarVersion()").that(version).isNotNull();
         assertWithMessage("Car.getCarVersion().toString()").that(version.toString())
-            .contains("name=Car.CAR_VERSION");
+                .contains("name=Car.CAR_VERSION");
     }
 
     @ApiTest(apis = {"android.car.Car#getPlatformVersion"})
@@ -167,7 +185,7 @@ public class CarTest {
 
         assertWithMessage("Car.getPlatformVersion()").that(version).isNotNull();
         assertWithMessage("Car.getPlatformVersion().toString()").that(version.toString())
-            .contains("name=Car.PLATFORM_VERSION");
+        .contains("name=Car.PLATFORM_VERSION");
     }
 
     @ApiTest(apis = {"android.car.Car#getPlatformVersion"})
