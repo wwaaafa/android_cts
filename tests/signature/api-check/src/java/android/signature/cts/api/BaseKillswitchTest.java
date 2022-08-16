0a9f052337c880827a6c10f500425dc406601033
Ramneek Kalra <ramneek.kalra@mediatek.com>
2022-08-16 15:41:23 +0800

    Adjust CtsHiddenApiKillswitchDebugClassTestCases test timeouts
    Because of increment of bootclasspath classes in Android T, the test execution costs more time. 
    Which causes some of the tests to timeout.
    
    Fix:
    Set the exception timeout to 15 minutes for some of the tests below for low end devices adaptation.
    
    Tests:
    "run cts -m CtsHiddenApiKillswitchDebugClassTestCases -t android.signature.cts.api.DebugClassKillswitchTest#testKillswitchMechanismFieldsThroughReflection"
    "run cts -m CtsHiddenApiKillswitchSdkListTestCases -t android.signature.cts.api.SdkListKillswitchTest#testKillswitchMechanismMethodsThroughJni"
    "run cts -m CtsHiddenApiKillswitchWildcardTestCases -t android.signature.cts.api.WildcardKillswitchTest#testKillswitchMechanismMethodsThroughJni"
    Bug: 242721832
    Change-Id: I803bc4f575cdb8530503216ff8816045832109ae

--- tests/signature/api-check/src/java/android/signature/cts/api/BaseKillswitchTest.java
+++ tests/signature/api-check/src/java/android/signature/cts/api/BaseKillswitchTest.java
@@ -49,12 +49,12 @@ public abstract class BaseKillswitchTest extends AbstractApiTest {
         doTestKillswitchMechanism(METHOD_FILTER, /* reflection= */ true, /* jni= */ false);
     }
 
-    @Test
+    @Test(timeout = 900000)
     public void testKillswitchMechanismMethodsThroughJni() {
         doTestKillswitchMechanism(METHOD_FILTER, /* reflection= */ false, /* jni= */ true);
     }
 
-    @Test
+    @Test(timeout = 900000)
     public void testKillswitchMechanismFieldsThroughReflection() {
         doTestKillswitchMechanism(FIELD_FILTER, /* reflection= */ true, /* jni= */ false);
     }
