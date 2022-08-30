734bb47a5af7647fad7d7fc66745e1faa17e450b
Iván Budnik <ivanbuper@google.com>
2022-08-30 13:07:33 +0000

    Revert "Add test to enforce component name belongs to caller app"
    This reverts commit af39c40ee5199c1eeb3dd94fcdb727d4ce2fcf45.
    
    Reason for revert: b/241798893. Note: this fix is not the root cause of the breakage. It just exposes a previously unknown bug. See linked bug.
    
    Change-Id: I26b811a2c6833b9da48c3b1a1c27ed888485d731
    Merged-In: Ic48badd6dc8efd876f09158e9e8549395ccc141d

--- tests/tests/media/misc/src/android/media/misc/cts/MediaSessionTest.java
+++ tests/tests/media/misc/src/android/media/misc/cts/MediaSessionTest.java
@@ -16,7 +16,6 @@
 package android.media.misc.cts;
 
 import static android.media.AudioAttributes.USAGE_GAME;
-import static android.media.cts.Utils.compareRemoteUserInfo;
 import static android.media.misc.cts.MediaSessionTestService.KEY_EXPECTED_QUEUE_SIZE;
 import static android.media.misc.cts.MediaSessionTestService.KEY_EXPECTED_TOTAL_NUMBER_OF_ITEMS;
 import static android.media.misc.cts.MediaSessionTestService.KEY_SESSION_TOKEN;
@@ -25,11 +24,11 @@ import static android.media.misc.cts.MediaSessionTestService.STEP_CLEAN_UP;
 import static android.media.misc.cts.MediaSessionTestService.STEP_SET_UP;
 import static android.media.misc.cts.MediaSessionTestService.TEST_SERIES_OF_SET_QUEUE;
 import static android.media.misc.cts.MediaSessionTestService.TEST_SET_QUEUE;
+import static android.media.cts.Utils.compareRemoteUserInfo;
 
 import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
@@ -52,12 +51,9 @@ import android.os.Looper;
 import android.os.Parcel;
 import android.os.Process;
 import android.platform.test.annotations.AppModeFull;
-import android.platform.test.annotations.AsbSecurityTest;
 import android.test.AndroidTestCase;
 import android.text.TextUtils;
 import android.view.KeyEvent;
-
-import com.android.compatibility.common.util.ApiTest;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -83,11 +79,6 @@ public class MediaSessionTest extends AndroidTestCase {
     private static final long TEST_QUEUE_ID = 12L;
     private static final long TEST_ACTION = 55L;
     private static final int TEST_TOO_MANY_SESSION_COUNT = 1000;
-
-    private static final String TEST_SESSION_TAG_FOREIGN_PACKAGE =
-            "test-session-tag-foreign-package";
-    private static final String TEST_FOREIGN_PACKAGE_NAME = "fakepackage";
-    private static final String TEST_FOREIGN_PACKAGE_CLASS = "com.fakepackage.media.FakeReceiver";
 
     private AudioManager mAudioManager;
     private Handler mHandler = new Handler(Looper.getMainLooper());
@@ -313,47 +304,6 @@ public class MediaSessionTest extends AndroidTestCase {
             assertTrue(mCallback.mOnSessionDestroyedCalled);
             // just call the callback once directly so it's marked as tested
             callback.onSessionDestroyed();
-        }
-    }
-
-    @ApiTest(apis = {"android.media.session.MediaSession#setMediaButtonBroadcastReceiver"})
-    @AsbSecurityTest(cveBugId = 238177121)
-    public void testSetMediaButtonBroadcastReceiver_withForeignPackageName_fails()
-            throws Exception {
-        Utils.assertMediaPlaybackStarted(getContext());
-
-        // Create Media Session
-        MyContextWrapper contextWrapper = new MyContextWrapper(getContext());
-        MediaSession mediaSession =
-                new MediaSession(contextWrapper, TEST_SESSION_TAG_FOREIGN_PACKAGE);
-
-        // Bypass client-side check
-        contextWrapper.mOverridePackageName = TEST_FOREIGN_PACKAGE_NAME;
-
-        try {
-            mediaSession.setMediaButtonBroadcastReceiver(
-                    new ComponentName(TEST_FOREIGN_PACKAGE_NAME, TEST_FOREIGN_PACKAGE_CLASS));
-            fail("Component name with different package name was registered.");
-        } catch (IllegalArgumentException ex) {
-            // Expected.
-        } finally {
-            mediaSession.release();
-        }
-    }
-
-    static class MyContextWrapper extends ContextWrapper {
-        String mOverridePackageName;
-
-        MyContextWrapper(Context base) {
-            super(base);
-        }
-
-        @Override
-        public String getPackageName() {
-            if (mOverridePackageName != null) {
-                return mOverridePackageName;
-            }
-            return super.getPackageName();
         }
     }
 
