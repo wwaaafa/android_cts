78e437a268b4022c0349908f795f7ace6449e7fb
Krishna C R <krishna.cr@ittiam.com>
2022-09-19 10:10:18 +0000

    Updated CTS test for Android Security b/174495520
    Bug: 174495520
    Bug: 179709762
    Test: Ran the new testcase on android-11.0.0_r34 with/without patch
    
    Change-Id: Ice74bc288ca2746eac4c941c6871bbbcf2ac1f71

--- hostsidetests/securitybulletin/src/android/security/cts/CVE_2021_0441.java
+++ hostsidetests/securitybulletin/src/android/security/cts/CVE_2021_0441.java
@@ -16,9 +16,12 @@
 
 package android.security.cts;
 
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeNoException;
+
 import android.platform.test.annotations.AsbSecurityTest;
 
-import com.android.sts.common.tradefed.testtype.StsExtraBusinessLogicHostTestBase;
+import com.android.sts.common.tradefed.testtype.NonRootSecurityTestCase;
 import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
 
@@ -27,25 +30,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(DeviceJUnit4ClassRunner.class)
-public class CVE_2021_0441 extends StsExtraBusinessLogicHostTestBase {
+public class CVE_2021_0441 extends NonRootSecurityTestCase {
     static final String TEST_PKG = "android.security.cts.CVE_2021_0441";
     static final String TEST_CLASS = TEST_PKG + "." + "DeviceTest";
     static final String TEST_APP = "CVE-2021-0441.apk";
 
     /**
      * b/174495520
+     * Vulnerable Module : com.google.android.mediaprovider
+     * Is Play managed   : Yes
      */
     @AsbSecurityTest(cveBugId = 174495520)
     @Test
-    public void testPocCVE_2021_0441() throws Exception {
-        ITestDevice device = getDevice();
-        uninstallPackage(device, TEST_PKG);
+    public void testPocCVE_2021_0441() {
+        try {
+            assumeFalse(moduleIsPlayManaged("com.google.android.mediaprovider"));
+            ITestDevice device = getDevice();
+            uninstallPackage(device, TEST_PKG);
 
-        AdbUtils.runCommandLine("input keyevent KEYCODE_WAKEUP", device);
-        AdbUtils.runCommandLine("input keyevent KEYCODE_MENU", device);
-        AdbUtils.runCommandLine("input keyevent KEYCODE_HOME", device);
+            AdbUtils.runCommandLine("input keyevent KEYCODE_WAKEUP", device);
+            AdbUtils.runCommandLine("input keyevent KEYCODE_MENU", device);
+            AdbUtils.runCommandLine("input keyevent KEYCODE_HOME", device);
 
-        installPackage(TEST_APP);
-        runDeviceTests(TEST_PKG, TEST_CLASS, "testCVE_2021_0441");
+            installPackage(TEST_APP);
+            runDeviceTests(TEST_PKG, TEST_CLASS, "testCVE_2021_0441");
+        } catch (Exception e) {
+            assumeNoException(e);
+        }
     }
 }
