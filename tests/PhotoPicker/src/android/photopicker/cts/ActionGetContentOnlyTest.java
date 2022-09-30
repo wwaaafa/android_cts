68cf226a55fd420a4e86bd021af24bf09ca4e3ba
Shubhi <shubhisaxena@google.com>
2022-09-30 08:18:44 +0000

    Add UI test for PhotoPickerSettingsActivity
    Add UI tests for PhotoPicker's Settings activity which can be launched
    from the overflow nemu in PhotoPickerActivity or the Settings app.
    
    Test: atest PhotoPickerSettingsTest
    Bug: 195009187
    Change-Id: I653b0119bd9c29b1964db80ce253957c495a353a

--- tests/PhotoPicker/src/android/photopicker/cts/ActionGetContentOnlyTest.java
+++ tests/PhotoPicker/src/android/photopicker/cts/ActionGetContentOnlyTest.java
@@ -91,7 +91,7 @@ public class ActionGetContentOnlyTest extends PhotoPickerBaseTest {
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("audio/*");
         mActivity.startActivityForResult(intent, REQUEST_CODE);
-        mDevice.waitForIdle();
+        sDevice.waitForIdle();
         // Should open documentsUi
         assertThatShowsDocumentsUiButtons();
 
@@ -107,7 +107,7 @@ public class ActionGetContentOnlyTest extends PhotoPickerBaseTest {
         intent.setType("image/*");
         intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"video/*", "audio/*"});
         mActivity.startActivityForResult(intent, REQUEST_CODE);
-        mDevice.waitForIdle();
+        sDevice.waitForIdle();
         // Should open documentsUi
         assertThatShowsDocumentsUiButtons();
 
@@ -131,7 +131,7 @@ public class ActionGetContentOnlyTest extends PhotoPickerBaseTest {
         intent.setType("image/*");
         mActivity.startActivityForResult(intent, REQUEST_CODE);
 
-        findAndClickBrowse(mDevice);
+        findAndClickBrowse(sDevice);
 
         findAndClickFilesInDocumentsUi(fileNameList);
 
@@ -157,7 +157,7 @@ public class ActionGetContentOnlyTest extends PhotoPickerBaseTest {
         intent.setType("image/*");
         mActivity.startActivityForResult(intent, REQUEST_CODE);
 
-        findAndClickBrowse(mDevice);
+        findAndClickBrowse(sDevice);
 
         findAndClickFilesInDocumentsUi(fileNameList);
 
@@ -210,13 +210,13 @@ public class ActionGetContentOnlyTest extends PhotoPickerBaseTest {
                 new UiObject(appList).waitForExists(SHORT_TIMEOUT)).isTrue();
 
         String photoPickerAppName = "Media";
-        UiObject mediaButton = mDevice.findObject(new UiSelector().text(photoPickerAppName));
+        UiObject mediaButton = sDevice.findObject(new UiSelector().text(photoPickerAppName));
 
         assertWithMessage("Timed out waiting for " + photoPickerAppName + " app icon to appear")
                 .that(new UiScrollable(appList).scrollIntoView(mediaButton)).isTrue();
-        mDevice.waitForIdle();
+        sDevice.waitForIdle();
 
-        clickAndWait(mDevice, mediaButton);
+        clickAndWait(sDevice, mediaButton);
     }
 
     private void assertThatShowsPickerUi() {
@@ -259,7 +259,7 @@ public class ActionGetContentOnlyTest extends PhotoPickerBaseTest {
     private void findAndClickSelect() throws Exception {
         final UiObject selectButton = new UiObject(new UiSelector().resourceId(
                 sDocumentsUiPackageName + ":id/action_menu_select"));
-        clickAndWait(mDevice, selectButton);
+        clickAndWait(sDevice, selectButton);
     }
 
     private void findAndClickFileInDocumentsUi(String fileName) throws Exception {
@@ -275,7 +275,7 @@ public class ActionGetContentOnlyTest extends PhotoPickerBaseTest {
             // Enforce to set the list mode
             // Because UiScrollable can't reach the real bottom (when WEB_LINKABLE_FILE item)
             // in grid mode when screen landscape mode
-            clickAndWait(mDevice, new UiObject(new UiSelector().resourceId(sDocumentsUiPackageName
+            clickAndWait(sDevice, new UiObject(new UiSelector().resourceId(sDocumentsUiPackageName
                     + ":id/sub_menu_list")));
         } catch (UiObjectNotFoundException ignored) {
             // Do nothing, already be in list mode.
@@ -297,9 +297,9 @@ public class ActionGetContentOnlyTest extends PhotoPickerBaseTest {
                 }
             }
 
-            mDevice.swipe(/* startX= */ mDevice.getDisplayWidth() / 2,
-                    /* startY= */ mDevice.getDisplayHeight() / 2,
-                    /* endX= */ mDevice.getDisplayWidth() / 2,
+            sDevice.swipe(/* startX= */ sDevice.getDisplayWidth() / 2,
+                    /* startY= */ sDevice.getDisplayHeight() / 2,
+                    /* endX= */ sDevice.getDisplayWidth() / 2,
                     /* endY= */ 0,
                     /* steps= */ 40);
         }
