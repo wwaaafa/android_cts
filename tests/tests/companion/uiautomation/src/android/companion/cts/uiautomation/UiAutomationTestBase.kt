de1f348788c55d90f157223617c983b6cceaef52
Raphael Kim <raphk@google.com>
2022-10-10 14:42:09 -0700

    Export common UI automation logic as android library
    Bug: 253307060
    Test: atest CtsCompanionDeviceManagerUiAutomationTestCases
    Change-Id: Ie5551bf81b3b2f115c26da8f56bf37a74c84cbca

--- tests/tests/companion/uiautomation/src/android/companion/cts/uiautomation/UiAutomationTestBase.kt
+++ tests/tests/companion/uiautomation/src/android/companion/cts/uiautomation/UiAutomationTestBase.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package android.companion.cts.uiautomation
 
 import android.Manifest
@@ -27,6 +43,7 @@ import android.companion.cts.common.RecordingCallback.OnFailure
 import android.companion.cts.common.SIMPLE_EXECUTOR
 import android.companion.cts.common.TestBase
 import android.companion.cts.common.assertEmpty
+import android.companion.cts.uicommon.CompanionDeviceManagerUi
 import android.content.Intent
 import android.net.MacAddress
 import android.os.Parcelable
@@ -86,18 +103,6 @@ open class UiAutomationTestBase(
         restoreDiscoveryTimeout()
 
         super.tearDown()
-    }
-
-    /**
-     * Execute UI flow to request user consent for permission transfer for a given association
-     * and grant permission.
-     */
-    fun requestPermissionTransferUserConsent(associationId: Int) {
-        val pendingUserConsent = cdm.buildPermissionTransferUserConsentIntent(associationId)
-        CompanionActivity.startIntentSender(pendingUserConsent!!)
-        confirmationUi.waitUntilSystemDataTransferConfirmationVisible()
-        confirmationUi.clickPositiveButton()
-        CompanionActivity.waitForActivityResult()
     }
 
     protected fun test_userRejected(
@@ -325,7 +330,8 @@ open class UiAutomationTestBase(
             }
 
     private fun restoreDiscoveryTimeout() = setSystemPropertyDuration(
-        ZERO, SYS_PROP_DEBUG_DISCOVERY_TIMEOUT)
+        ZERO, SYS_PROP_DEBUG_DISCOVERY_TIMEOUT
+    )
 
     companion object {
         /**
