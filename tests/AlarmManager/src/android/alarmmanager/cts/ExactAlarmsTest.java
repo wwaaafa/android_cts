352d579c5ce0a3aa20a88ec9ea27506282e4c427
Michael Groover <mpgroover@google.com>
2022-10-28 19:29:32 -0500

    Add unaudited exported flag to exposed runtime receivers
    Android T allows apps to declare a runtime receiver as not exported
    by invoking registerReceiver with a new RECEIVER_NOT_EXPORTED flag;
    receivers registered with this flag will only receive broadcasts from
    the platform and the app itself. However to ensure developers can
    properly protect their receivers, all apps targeting U or later
    registering a receiver for non-system broadcasts must specify either
    the exported or not exported flag when invoking #registerReceiver;
    if one of these flags is not provided, the platform will throw a
    SecurityException. This commit updates all the exposed receivers
    with a new RECEIVER_EXPORTED_UNAUDITED flag to maintain the existing
    behavior of exporting the receiver while also flagging the receiver
    for audit before the U release.
    
    Bug: 234659204
    Test: Build
    Change-Id: I5084cd352c3639576110ee780a03332db2bc34e5

--- tests/AlarmManager/src/android/alarmmanager/cts/ExactAlarmsTest.java
+++ tests/AlarmManager/src/android/alarmmanager/cts/ExactAlarmsTest.java
@@ -414,7 +414,7 @@ public class ExactAlarmsTest {
                 alarmLatch.countDown();
             }
         };
-        sContext.registerReceiver(receiver, filter);
+        sContext.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED_UNAUDITED);
         try {
             Thread.sleep(5_000);
             assertTrue("AlarmClock expiration not reported",
@@ -643,7 +643,7 @@ public class ExactAlarmsTest {
                 latch.countDown();
             }
         };
-        sContext.registerReceiver(receiver, filter);
+        sContext.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED_UNAUDITED);
         try {
             Log.d(TAG, "Granting the appop");
             setAppOp(TEST_APP_PACKAGE, AppOpsManager.MODE_ALLOWED);
@@ -684,7 +684,7 @@ public class ExactAlarmsTest {
                 latch.countDown();
             }
         };
-        sContext.registerReceiver(receiver, filter);
+        sContext.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED_UNAUDITED);
         try {
             Log.d(TAG, "Removing from deny list");
             mDeviceConfigHelper.without("exact_alarm_deny_list").commitAndAwaitPropagation();
