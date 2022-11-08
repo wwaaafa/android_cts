f83fc23ad4410f2d6cdbdeafd4a3349137d6c3c1
Aaqib Ismail <aaqibismail@google.com>
2022-11-08 15:25:19 -0800

    Add CTS tests for STEERING_WHEEL_HEIGHT_MOVE
    Bug: 257267721
    Test: atest VtsHalAutomotiveVehicle_TargetTest
          atest CtsCarTestCases:CarPropertyManagerTest
          atest CtsCarTestCases:VehiclePropertyIdsTest
    Change-Id: I248c1729a7eced88e8f963ff28b3656b9f5f0ec7

--- tests/tests/car/src/android/car/cts/VehiclePropertyIdsTest.java
+++ tests/tests/car/src/android/car/cts/VehiclePropertyIdsTest.java
@@ -300,6 +300,8 @@ public class VehiclePropertyIdsTest extends AbstractCarLessTestCase {
                 .isEqualTo("STEERING_WHEEL_DEPTH_MOVE");
         assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.STEERING_WHEEL_DEPTH_POS))
                 .isEqualTo("STEERING_WHEEL_DEPTH_POS");
+        assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.STEERING_WHEEL_HEIGHT_MOVE))
+                .isEqualTo("STEERING_WHEEL_HEIGHT_MOVE");
         assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.STEERING_WHEEL_HEIGHT_POS))
                 .isEqualTo("STEERING_WHEEL_HEIGHT_POS");
         assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.VEHICLE_MAP_SERVICE))
