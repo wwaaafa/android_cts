7e237f76c353a129b778510ecd4bf4e99bccba2a
Calvin Pan <calvinpan@google.com>
2022-11-14 13:17:12 +0800

    Add cts for gender
    Bug: 259175845
    Test: atest
    Change-Id: I557a4d410e8dfdede219f89245a08d003e67e50f

--- tests/framework/base/windowmanager/util/src/android/server/wm/ActivityManagerTestBase.java
+++ tests/framework/base/windowmanager/util/src/android/server/wm/ActivityManagerTestBase.java
@@ -2267,7 +2267,7 @@ public abstract class ActivityManagerTestBase {
     }
 
     /** Assert the activity is either relaunched or received configuration changed. */
-    static void assertActivityLifecycle(ComponentName activityName, boolean relaunched) {
+    protected static void assertActivityLifecycle(ComponentName activityName, boolean relaunched) {
         Condition.<String>waitForResult(
                 activityName + (relaunched ? " relaunched" : " config changed"),
                 condition -> condition
