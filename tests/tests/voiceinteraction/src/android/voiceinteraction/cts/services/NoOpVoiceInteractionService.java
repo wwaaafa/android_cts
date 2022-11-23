d209c35ebb7625bdf27392bc8406524067ee5d3b
Joanne <joannechung@google.com>
2022-11-23 22:49:39 +0800

    Refine structure for VoiceInteractionRoleTest
    It is unnecessary to separate the service to another folder for
    VoiceInteractionRoleTest, so move NoOpVoiceInteractionService back to
    test folder.
    
    Also move SettingsActivity and MainRecognitionService back to test
    folder, the original files still need to keep because still have some
    references. Will remove these files after refactor done.
    
    Bug: 249287756
    Test: atest CtsVoiceInteractionTestCases
    Change-Id: I4fee979673ac11a47a8b2de84411b8287de0594e

--- tests/tests/voiceinteraction/src/android/voiceinteraction/cts/services/NoOpVoiceInteractionService.java
+++ tests/tests/voiceinteraction/src/android/voiceinteraction/cts/services/NoOpVoiceInteractionService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package android.voiceinteraction.service;
+package android.voiceinteraction.cts.services;
 
 import android.service.voice.VoiceInteractionService;
 
