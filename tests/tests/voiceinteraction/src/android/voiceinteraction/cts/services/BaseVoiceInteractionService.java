1f6beb57e571288493f7463ca10d50b633d75a83
Joanne <joannechung@google.com>
2022-11-24 23:03:40 +0800

    Refactor AlwaysOnHotwordDetectorTest(1/2)
    Currently, the voice cts communication across three packages. It
    makes the test hard to read and we need to use broadcast, service
    to let the data can pass between these thress packages.
    
    To reduce test complexability, we will try to make the test
    communication in the same test package that can access service
    variables or methods easier.
    
    In order to allow test can access service instance directly, the test
    needs to wait service onReady() be called then it can be guaranteed
    to get the service instance. Currently, the test uses the
    SettingsStateChangerRule, it is hard to catch when the onReady() is
    called. The possible solution is to set a CountDownLatch before the
    test changes to test VoiceInteractionService. In this change, we
    introduce a TestVoiceInteracionServiceConnectedRule that will set a
    CountDownLatch before changing to test service. We also introduce a
    base VoiceInteractionService that controlls the service connection
    state.
    
    The rule here will be used in the next AlwaysOnHotwordDetectorTest
    refactor.
    
    Bug: 249287756
    Test: atest CtsVoiceInteractionTestCases
    Test: add a local test and make sure the connect CountDownLatch works
    fine
    
    Change-Id: Ibce4fac0ec457009b37bfb1b16d132757f2df4ad

---
+++ tests/tests/voiceinteraction/src/android/voiceinteraction/cts/services/BaseVoiceInteractionService.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.voiceinteraction.cts.services;
+
+import android.service.voice.VoiceInteractionService;
+import android.util.Log;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The base {@link VoiceInteractionService} that provides the common methods for subclass.
+ */
+public abstract class BaseVoiceInteractionService extends VoiceInteractionService {
+
+    public static final String SERVICE_PACKAGE = "android.voiceinteraction.cts";
+
+    private final String mTag = getClass().getSimpleName();
+
+    // The service instance
+    public static VoiceInteractionService sService;
+    // The timeout to wait for async result
+    public static final long WAIT_TIMEOUT_IN_MS = 5_000;
+    // The CountDownLatch waits for service connect
+    public static CountDownLatch sConnectLatch;
+
+    public static CountDownLatch sDisconnectLatch;
+
+    @Override
+    public void onReady() {
+        super.onReady();
+        Log.d(mTag, "onReady()");
+        sService = this;
+        if (sConnectLatch != null) {
+            sConnectLatch.countDown();
+        }
+    }
+
+    @Override
+    public void onShutdown() {
+        super.onShutdown();
+        Log.d(mTag, "onShutdown()");
+        if (sDisconnectLatch != null) {
+            sDisconnectLatch.countDown();
+        }
+    }
+
+    /**
+     * Reset the static variables of this service.
+     */
+    public static void resetStaticValues() {
+        sService = null;
+        sConnectLatch = null;
+        sDisconnectLatch = null;
+    }
+
+    /**
+     * Init the CountDownLatch that is used to wait for service onReady() and onShutdown().
+     */
+    public static void initServiceConnectionLatches() {
+        sConnectLatch = new CountDownLatch(1);
+        sDisconnectLatch = new CountDownLatch(1);
+    }
+
+    /**
+     * Wait for service onReady().
+     */
+    public static void waitServiceConnect() throws InterruptedException {
+        if (sConnectLatch == null) {
+            throw new AssertionError("Should init connect CountDownLatch");
+        }
+        if (!sConnectLatch.await(WAIT_TIMEOUT_IN_MS, TimeUnit.MILLISECONDS)) {
+            throw new AssertionError("VoiceInteractionService doesn't start.");
+        }
+        sConnectLatch = null;
+    }
+
+    /**
+     * Wait for service onShutdown().
+     */
+    public static void waitServiceDisconnect() throws InterruptedException {
+        if (sDisconnectLatch == null) {
+            throw new AssertionError("Should init disconnect CountDownLatch");
+        }
+        if (!sDisconnectLatch.await(WAIT_TIMEOUT_IN_MS, TimeUnit.MILLISECONDS)) {
+            throw new AssertionError("VoiceInteractionService doesn't shut down.");
+        }
+        sDisconnectLatch = null;
+    }
+
+    public static VoiceInteractionService getService() {
+        return sService;
+    }
+}
