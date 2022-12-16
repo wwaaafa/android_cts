db3e1b0403b81bdf5443c010eee8ad00ac7adc69
shrikar <shrikar@google.com>
2022-12-16 03:09:05 +0000

    Added SEAT_WALK_IN_POS to CTS tests.
    Bug: 262765640
    Test: atest CarPropertyManagerTest
    Test: atest VehiclePropertyIdsTest
    Change-Id: I12a4df9a3c6d91794a6e4b15f5cd1093379c92d2

--- tests/tests/car/src/android/car/cts/VehiclePropertyIdsTest.java
+++ tests/tests/car/src/android/car/cts/VehiclePropertyIdsTest.java
@@ -310,6 +310,8 @@ public class VehiclePropertyIdsTest extends AbstractCarLessTestCase {
                 .isEqualTo("SEAT_LUMBAR_VERTICAL_POS");
         assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.SEAT_LUMBAR_VERTICAL_MOVE))
                 .isEqualTo("SEAT_LUMBAR_VERTICAL_MOVE");
+        assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.SEAT_WALK_IN_POS))
+                .isEqualTo("SEAT_WALK_IN_POS");
         assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.WINDOW_LOCK))
                 .isEqualTo("WINDOW_LOCK");
         assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.WINDOW_MOVE))
