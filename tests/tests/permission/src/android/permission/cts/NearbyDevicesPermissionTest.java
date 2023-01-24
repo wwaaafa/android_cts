eb1576b3a01313aa4c46819a0013812589e5d3e7
Manidhar <manidhar.mulaparthi@elektrobit.com>
2023-01-24 14:28:09 +0000

    CtsPermission3TestCases & CtsPermissionTestCases failures due to BLE feature removal
    Cts test cases from CtsPermission3TestCases & CtsPermissionTestCases fails due to FEATURE_BLUETOOTH_LE unavailability.
    Checking the feature availability before running the test case using assumeTrue api.
    
    Bug: 265135095
    Test: Executed CtsPermission3TestCases & CtsPermissionTestCases module test class
    Change-Id: I56d3d1189df96bf026e25cce2b77010c68fdc485

--- tests/tests/permission/src/android/permission/cts/NearbyDevicesPermissionTest.java
+++ tests/tests/permission/src/android/permission/cts/NearbyDevicesPermissionTest.java
@@ -51,7 +51,6 @@ import com.android.compatibility.common.util.CddTest;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -114,6 +113,8 @@ public class NearbyDevicesPermissionTest {
     @Test
     @CddTest(requirement="7.4.3/C-6-1")
     public void testRequestBluetoothPermission30_Default() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_30);
         assertScanBluetoothResult(Result.EMPTY);
     }
@@ -121,6 +122,8 @@ public class NearbyDevicesPermissionTest {
     @Test
     @CddTest(requirement="7.4.3/C-6-1")
     public void testRequestBluetoothPermission30_GrantLocation() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_30);
         grantPermission(TEST_APP_PKG, ACCESS_FINE_LOCATION);
         grantPermission(TEST_APP_PKG, ACCESS_BACKGROUND_LOCATION);
@@ -137,6 +140,8 @@ public class NearbyDevicesPermissionTest {
     @Test
     @CddTest(requirement="7.4.3/C-6-1")
     public void testRequestBluetoothPermission31_GrantNearby() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_31);
         grantPermission(TEST_APP_PKG, BLUETOOTH_CONNECT);
         grantPermission(TEST_APP_PKG, BLUETOOTH_SCAN);
@@ -155,6 +160,8 @@ public class NearbyDevicesPermissionTest {
     @Test
     @CddTest(requirement="7.4.3/C-6-1")
     public void testRequestBluetoothPermission31_GrantNearby_GrantLocation() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_31);
         grantPermission(TEST_APP_PKG, BLUETOOTH_CONNECT);
         grantPermission(TEST_APP_PKG, BLUETOOTH_SCAN);
@@ -173,6 +180,8 @@ public class NearbyDevicesPermissionTest {
     @Test
     @CddTest(requirement="7.4.3/C-6-1")
     public void testRequestBluetoothPermissionNeverForLocation31_GrantNearby() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_NEVER_FOR_LOCATION_31);
         grantPermission(TEST_APP_PKG, BLUETOOTH_CONNECT);
         grantPermission(TEST_APP_PKG, BLUETOOTH_SCAN);
@@ -192,6 +201,8 @@ public class NearbyDevicesPermissionTest {
     @CddTest(requirement="7.4.3/C-6-1")
     public void testRequestBluetoothPermissionNeverForLocation31_GrantNearby_GrantLocation()
             throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_NEVER_FOR_LOCATION_31);
         grantPermission(TEST_APP_PKG, BLUETOOTH_CONNECT);
         grantPermission(TEST_APP_PKG, BLUETOOTH_SCAN);
@@ -202,6 +213,8 @@ public class NearbyDevicesPermissionTest {
 
     @Test
     public void testRequestBluetoothPermission31_OnBehalfOfDisavowingApp() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_31);
         install(APK_BLUETOOTH_NEVER_FOR_LOCATION_NO_PROVIDER);
         grantPermission(TEST_APP_PKG, BLUETOOTH_CONNECT);
@@ -218,6 +231,8 @@ public class NearbyDevicesPermissionTest {
      */
     @Test
     public void testRequestBluetoothPermission_Default_Upgrade() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_30);
         assertScanBluetoothResult(Result.EMPTY);
 
@@ -238,6 +253,8 @@ public class NearbyDevicesPermissionTest {
      */
     @Test
     public void testRequestBluetoothPermission_GrantLocation_Upgrade() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_30);
         grantPermission(TEST_APP_PKG, ACCESS_FINE_LOCATION);
         grantPermission(TEST_APP_PKG, ACCESS_BACKGROUND_LOCATION);
@@ -255,6 +272,8 @@ public class NearbyDevicesPermissionTest {
      */
     @Test
     public void testRequestBluetoothPermission_Downgrade() throws Throwable {
+        assumeTrue(supportsBluetoothLe());
+
         install(APK_BLUETOOTH_31);
         grantPermission(TEST_APP_PKG, BLUETOOTH_CONNECT);
         grantPermission(TEST_APP_PKG, BLUETOOTH_SCAN);
@@ -288,6 +307,10 @@ public class NearbyDevicesPermissionTest {
 
     private boolean supportsBluetooth() {
         return mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
+    }
+
+    private boolean supportsBluetoothLe() {
+        return mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
     }
 
     private void enableTestMode() {
