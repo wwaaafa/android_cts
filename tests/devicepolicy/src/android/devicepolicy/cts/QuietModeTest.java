63db78dfaf929bcf946e642a95960600f4726fce
Pavel Grafov <pgrafov@google.com>
2023-01-24 15:37:12 +0000

    Test quiet mode when profiles are kept running
    Bug: 258823777
    Test: btest android.devicepolicy.cts.QuietModeTest
    Change-Id: I2fd6ac3fd06e1a5fa3d63114b21775fabd110322

--- tests/devicepolicy/src/android/devicepolicy/cts/QuietModeTest.java
+++ tests/devicepolicy/src/android/devicepolicy/cts/QuietModeTest.java
@@ -23,6 +23,10 @@ import static com.android.queryable.queries.ActivityQuery.activity;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -34,6 +38,9 @@ import com.android.bedstead.harrier.annotations.EnsureHasPermission;
 import com.android.bedstead.harrier.annotations.EnsureHasWorkProfile;
 import com.android.bedstead.harrier.annotations.Postsubmit;
 import com.android.bedstead.nene.TestApis;
+import com.android.bedstead.nene.users.UserReference;
+import com.android.bedstead.nene.utils.Poll;
+import com.android.bedstead.nene.utils.ShellCommand;
 import com.android.bedstead.testapp.TestApp;
 import com.android.bedstead.testapp.TestAppActivityReference;
 import com.android.bedstead.testapp.TestAppInstance;
@@ -57,12 +64,15 @@ public class QuietModeTest {
             .contains(activity().where().exported().isTrue())
             .get();
 
+    private static final Context sContext = TestApis.context().instrumentedContext();
+
     @Postsubmit(reason = "new test")
     @EnsureHasWorkProfile
     @EnsureHasPermission(INTERACT_ACROSS_USERS_FULL)
     @Test
     public void startActivityInQuietProfile_quietModeDialogShown() throws Exception {
-        try (TestAppInstance instance = sTestApp.install(sDeviceState.workProfile())) {
+        UserReference workProfile = sDeviceState.workProfile();
+        try (TestAppInstance instance = sTestApp.install(workProfile)) {
             // Override "Turn on work apps" dialog title to avoid depending on a particular string.
             TestApis.devicePolicy().resources().strings().set(
                     "Core.UNLAUNCHABLE_APP_WORK_PAUSED_TITLE", R.string.test_string_1);
@@ -70,14 +80,12 @@ public class QuietModeTest {
             TestAppActivityReference activityReference =
                     instance.activities().query().whereActivity().exported().isTrue().get();
 
-            sDeviceState.workProfile().setQuietMode(true);
+            workProfile.setQuietMode(true);
 
             Intent intent = new Intent()
                     .addFlags(FLAG_ACTIVITY_NEW_TASK)
                     .setComponent(activityReference.component().componentName());
-            TestApis.context().instrumentedContext()
-                    .startActivityAsUser(
-                            intent, new Bundle(), sDeviceState.workProfile().userHandle());
+            sContext.startActivityAsUser(intent, new Bundle(), workProfile.userHandle());
 
             assertThat(TestApis.ui().device()
                     .findObject(new UiSelector().text("test string 1"))
@@ -85,7 +93,58 @@ public class QuietModeTest {
         } finally {
             TestApis.devicePolicy().resources().strings()
                     .reset("Core.UNLAUNCHABLE_APP_WORK_PAUSED_TITLE");
-            sDeviceState.workProfile().setQuietMode(false);
+            workProfile.setQuietMode(false);
         }
+    }
+
+    @Postsubmit(reason = "new test")
+    @EnsureHasWorkProfile(dpcIsPrimary = true)
+    @EnsureHasPermission(INTERACT_ACROSS_USERS_FULL)
+    @Test
+    public void quietMode_profileKeptRunning() throws Exception {
+        assumeTrue("Keep profiles available feature not enabled", keepProfilesRunningEnabled());
+        UserReference workProfile = sDeviceState.workProfile();
+        try {
+            workProfile.setQuietMode(true);
+
+            // If user is being stopped, let it finish.
+            TestApis.broadcasts().waitForBroadcastBarrier("Ensure asynchronous work done");
+
+            assertThat(workProfile.isRunning()).isTrue();
+            // This requires running code in the profile user and will throw an exception if the
+            // user is stopped.
+            assertThat(sDeviceState.dpc().userManager().isManagedProfile()).isTrue();
+            // The DPC should be suspended in quiet mode if the profile is kept running.
+            assertThat(sContext.getPackageManager().isPackageSuspendedForUser(
+                    sDeviceState.dpc().packageName(), workProfile.id())).isTrue();
+        } finally {
+            workProfile.setQuietMode(false);
+        }
+    }
+
+    @Postsubmit(reason = "new test")
+    @EnsureHasWorkProfile(dpcIsPrimary = true)
+    @EnsureHasPermission(INTERACT_ACROSS_USERS_FULL)
+    @Test
+    public void quietMode_profileStopped() throws Exception {
+        assumeFalse("Keep profiles available feature is enabled", keepProfilesRunningEnabled());
+        UserReference workProfile = sDeviceState.workProfile();
+        try {
+            workProfile.setQuietMode(true);
+
+            // Profile should be stopped.
+            Poll.forValue("profile running", workProfile::isRunning).toBeEqualTo(false)
+                    .errorOnFail().await();
+
+            // The DPC shouldn't be suspended.
+            assertThat(sDeviceState.dpc().testApp().pkg().isSuspended(workProfile)).isFalse();
+        } finally {
+            workProfile.setQuietMode(false);
+        }
+    }
+
+    private boolean keepProfilesRunningEnabled() throws Exception {
+        return ShellCommand.builder("dumpsys device_policy").execute()
+                .contains("Keep profiles running: true");
     }
 }
