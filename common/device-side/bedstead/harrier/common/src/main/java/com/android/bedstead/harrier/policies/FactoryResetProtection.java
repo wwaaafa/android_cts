b45bdccdc483df044335ec547188e8aa32c33f59
Jonathan Scott <scottjonathan@google.com>
2023-02-14 19:31:56 +0000

    Fix test annotations.
    Test: btest CtsDevicePolicyTestCases
    Change-Id: If14743a86ecaa8dd8f0a56435d0f3ea267ab6785

--- common/device-side/bedstead/harrier/common/src/main/java/com/android/bedstead/harrier/policies/FactoryResetProtection.java
+++ common/device-side/bedstead/harrier/common/src/main/java/com/android/bedstead/harrier/policies/FactoryResetProtection.java
@@ -17,6 +17,7 @@
 package com.android.bedstead.harrier.policies;
 
 import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy.APPLIED_BY_DEVICE_OWNER;
+import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy.APPLIED_BY_DPM_ROLE_HOLDER;
 import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy.APPLIED_BY_ORGANIZATION_OWNED_PROFILE_OWNER_PROFILE;
 import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy.APPLIED_BY_PARENT_INSTANCE_OF_PROFILE_OWNER_PROFILE;
 import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy.APPLIES_GLOBALLY;
@@ -32,8 +33,8 @@ import com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy;
  * {@code DevicePolicyManager#getFactoryResetProtection}.
  */
 @EnterprisePolicy(dpc =
-        APPLIED_BY_DEVICE_OWNER | APPLIED_BY_ORGANIZATION_OWNED_PROFILE_OWNER_PROFILE | APPLIES_GLOBALLY
-                | CANNOT_BE_APPLIED_BY_ROLE_HOLDER,
+        APPLIED_BY_DEVICE_OWNER | APPLIED_BY_ORGANIZATION_OWNED_PROFILE_OWNER_PROFILE |
+                APPLIED_BY_DPM_ROLE_HOLDER | APPLIES_GLOBALLY |  CANNOT_BE_APPLIED_BY_ROLE_HOLDER,
         permissions =
         @EnterprisePolicy.Permission(appliedWith = MANAGE_DEVICE_POLICY_FACTORY_RESET, appliesTo = APPLIES_GLOBALLY))
 public final class FactoryResetProtection {
