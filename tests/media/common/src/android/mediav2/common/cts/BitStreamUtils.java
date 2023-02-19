b5f0baa9cd7337805ba4e1fb8adb2f650e6232e4
Ram Mohan M <ram.mohan@ittiam.com>
2023-02-19 14:41:54 +0530

    mediav2 CTS: Refactor Bitstream Utils
    Bug: 270019381
    Test: atest android.videocodec.cts.VideoEncoderIntraFrameIntervalTest \
    android.videocodec.cts.VideoEncoderMaxBFrameTest
    
    Change-Id: I6fb96d92344aef409b91391407c3852944118f78

--- tests/media/common/src/android/mediav2/common/cts/BitStreamUtils.java
+++ tests/media/common/src/android/mediav2/common/cts/BitStreamUtils.java
@@ -28,7 +28,9 @@ import java.nio.ByteBuffer;
 
 /**
  * This class contains utility functions that parse compressed bitstream and returns metadata
- * necessary for validation.
+ * necessary for validation. This is by no means a thorough parser that is capable of parsing all
+ * syntax elements of a bitstream. This is designed to handle only the requirements of mediav2
+ * test suite.
  * <p>
  * Currently this class hosts utils that can,
  * <ul>
@@ -36,18 +38,12 @@ import java.nio.ByteBuffer;
  * </ul>
  */
 public class BitStreamUtils {
-    private static final int NO_NAL_UNIT_FOUND = 0;
-    private static final int TRAIL_N = 0;
-    private static final int RASL_R = 9;
-    private static final int BLA_W_LP = 16;
-    private static final int RSV_IRAP_VCL23 = 23;
-
     static class ParsableBitArray {
-        private final byte[] mData;
-        private final int mOffset;
-        private final int mLimit;
-        private int mCurrByteOffset;
-        private int mCurrBitOffset;
+        protected final byte[] mData;
+        protected final int mOffset;
+        protected final int mLimit;
+        protected int mCurrByteOffset;
+        protected int mCurrBitOffset;
 
         ParsableBitArray(byte[] data, int offset, int limit) {
             mData = data;
@@ -58,13 +54,6 @@ public class BitStreamUtils {
         }
 
         public boolean readBit() {
-            // emulation prevention
-            if (mCurrBitOffset == 0 && (mCurrByteOffset - 2 > mOffset)
-                    && mData[mCurrByteOffset] == (byte) 0x03
-                    && mData[mCurrByteOffset - 1] == (byte) 0x00
-                    && mData[mCurrByteOffset - 2] == (byte) 0x00) {
-                mCurrByteOffset++;
-            }
             if (mCurrByteOffset >= mLimit) {
                 throw new ArrayIndexOutOfBoundsException(
                         String.format("Accessing bytes at offset %d, buffer limit %d",
@@ -90,6 +79,24 @@ public class BitStreamUtils {
             }
             return value;
         }
+    }
+
+    static class NalParsableBitArray extends ParsableBitArray {
+        NalParsableBitArray(byte[] data, int offset, int limit) {
+            super(data, offset, limit);
+        }
+
+        @Override
+        public boolean readBit() {
+            // emulation prevention
+            if (mCurrBitOffset == 0 && (mCurrByteOffset - 2 > mOffset)
+                    && mData[mCurrByteOffset] == (byte) 0x03
+                    && mData[mCurrByteOffset - 1] == (byte) 0x00
+                    && mData[mCurrByteOffset - 2] == (byte) 0x00) {
+                mCurrByteOffset++;
+            }
+            return super.readBit();
+        }
 
         public int readUEV() {
             int leadingZeros = 0;
@@ -100,103 +107,143 @@ public class BitStreamUtils {
         }
     }
 
-    private static int getNalUnitStartOffset(byte[] dataArray, int pos, int limit) {
-        if ((pos + 3) < limit && dataArray[pos] == 0 && dataArray[pos + 1] == 0
-                && dataArray[pos + 2] == 1) {
-            return 3;
-        } else if ((pos + 4) < limit && dataArray[pos] == 0 && dataArray[pos + 1] == 0
-                && dataArray[pos + 2] == 0 && dataArray[pos + 3] == 1) {
-            return 4;
+    public abstract static class ParserBase {
+        protected byte[] mData;
+        protected int mOffset;
+        protected int mLimit;
+
+        protected void set(byte[] data, int offset, int limit) {
+            mData = data;
+            mOffset = offset;
+            mLimit = limit;
         }
-        return NO_NAL_UNIT_FOUND;
+
+        public abstract int getFrameType();
     }
 
-    private static int getAvcNalUnitType(byte[] dataArray, int nalUnitOffset) {
-        return dataArray[nalUnitOffset] & 0x1F;
-    }
+    static class AvcParser extends ParserBase {
+        private static final int NO_NAL_UNIT_FOUND = -1;
 
-    private static int parseAvcCodecSliceNalUnit(byte[] dataArray, int offset, int limit) {
-        ParsableBitArray bitArray = new ParsableBitArray(dataArray, offset, limit);
+        private int getNalUnitStartOffset(byte[] dataArray, int start, int limit) {
+            for (int pos = start; pos + 3 < limit; pos++) {
+                if ((pos + 3) < limit && dataArray[pos] == 0 && dataArray[pos + 1] == 0
+                        && dataArray[pos + 2] == 1) {
+                    return pos + 3;
+                } else if ((pos + 4) < limit && dataArray[pos] == 0 && dataArray[pos + 1] == 0
+                        && dataArray[pos + 2] == 0 && dataArray[pos + 3] == 1) {
+                    return pos + 4;
+                }
+            }
+            return NO_NAL_UNIT_FOUND;
+        }
 
-        bitArray.readBits(8); // forbidden zero bit + nal_ref_idc + nal_unit_type
-        bitArray.readUEV(); // first_mb_in_slice
-        int sliceType = bitArray.readUEV();
-        if (sliceType % 5 == 0) {
-            return PICTURE_TYPE_P;
-        } else if (sliceType % 5 == 1) {
-            return PICTURE_TYPE_B;
-        } else if (sliceType % 5 == 2) {
-            return PICTURE_TYPE_I;
-        } else {
+        private static int getNalUnitType(byte[] dataArray, int nalUnitOffset) {
+            return dataArray[nalUnitOffset] & 0x1F;
+        }
+
+        @Override
+        public int getFrameType() {
+            for (int pos = mOffset; pos < mLimit; ) {
+                int offset = getNalUnitStartOffset(mData, pos, mLimit);
+                if (offset == NO_NAL_UNIT_FOUND) return PICTURE_TYPE_UNKNOWN;
+                int nalUnitType = getNalUnitType(mData, offset);
+                if (nalUnitType == 1 || nalUnitType == 2 || nalUnitType == 5) {  // coded slice
+                    NalParsableBitArray bitArray = new NalParsableBitArray(mData, offset, mLimit);
+                    bitArray.readBits(8); // forbidden zero bit + nal_ref_idc + nal_unit_type
+                    bitArray.readUEV(); // first_mb_in_slice
+                    int sliceType = bitArray.readUEV();
+                    if (sliceType % 5 == 0) {
+                        return PICTURE_TYPE_P;
+                    } else if (sliceType % 5 == 1) {
+                        return PICTURE_TYPE_B;
+                    } else if (sliceType % 5 == 2) {
+                        return PICTURE_TYPE_I;
+                    } else {
+                        return PICTURE_TYPE_UNKNOWN;
+                    }
+                }
+                pos = offset;
+            }
             return PICTURE_TYPE_UNKNOWN;
         }
     }
 
-    private static int getHevcNalUnitType(byte[] dataArray, int nalUnitOffset) {
-        return (dataArray[nalUnitOffset] & 0x7E) >> 1;
-    }
+    static class HevcParser extends ParserBase {
+        private static final int NO_NAL_UNIT_FOUND = -1;
+        private static final int TRAIL_N = 0;
+        private static final int RASL_R = 9;
+        private static final int BLA_W_LP = 16;
+        private static final int RSV_IRAP_VCL23 = 23;
 
-    private static int parseHevcSliceSegmentNalUnit(byte[] dataArray, int offset, int limit,
-            int nalUnitType) {
-        // nal_unit_type values from H.265/HEVC Table 7-1.
-        ParsableBitArray bitArray = new ParsableBitArray(dataArray, offset, limit);
-
-        bitArray.readBits(16); // nal_unit_header
-
-        // Parsing slice_segment_header values from H.265/HEVC Table 7.3.6.1
-        boolean first_slice_segment = bitArray.readBit(); // first_slice_segment_in_pic_flag
-        if (!first_slice_segment) return -1;
-        if (nalUnitType >= BLA_W_LP && nalUnitType <= RSV_IRAP_VCL23) {
-            bitArray.readBit();  // no_output_of_prior_pics_flag
+        private int getNalUnitStartOffset(byte[] dataArray, int start, int limit) {
+            for (int pos = start; pos + 3 < limit; pos++) {
+                if ((pos + 3) < limit && dataArray[pos] == 0 && dataArray[pos + 1] == 0
+                        && dataArray[pos + 2] == 1) {
+                    return pos + 3;
+                } else if ((pos + 4) < limit && dataArray[pos] == 0 && dataArray[pos + 1] == 0
+                        && dataArray[pos + 2] == 0 && dataArray[pos + 3] == 1) {
+                    return pos + 4;
+                }
+            }
+            return NO_NAL_UNIT_FOUND;
         }
-        bitArray.readUEV(); // slice_pic_parameter_set_id
-        // Assumes num_extra_slice_header_bits element of PPS data to be 0
-        int sliceType = bitArray.readUEV();
-        if (sliceType == 0) {
-            return PICTURE_TYPE_B;
-        } else if (sliceType == 1) {
-            return PICTURE_TYPE_P;
-        } else if (sliceType == 2) {
-            return PICTURE_TYPE_I;
-        } else {
+
+        private static int getNalUnitType(byte[] dataArray, int nalUnitOffset) {
+            return (dataArray[nalUnitOffset] & 0x7E) >> 1;
+        }
+
+        @Override
+        public int getFrameType() {
+            for (int pos = mOffset; pos < mLimit; ) {
+                int offset = getNalUnitStartOffset(mData, pos, mLimit);
+                if (offset == NO_NAL_UNIT_FOUND) return PICTURE_TYPE_UNKNOWN;
+                int nalUnitType = getNalUnitType(mData, offset);
+                if ((nalUnitType >= TRAIL_N && nalUnitType <= RASL_R) || (nalUnitType >= BLA_W_LP
+                        && nalUnitType <= RSV_IRAP_VCL23)) { // codec slice
+                    NalParsableBitArray bitArray = new NalParsableBitArray(mData, offset, mLimit);
+                    bitArray.readBits(16); // nal_unit_header
+
+                    // Parsing slice_segment_header values from H.265/HEVC Table 7.3.6.1
+                    boolean firstSliceSegmentInPicFlag = bitArray.readBit();
+                    if (!firstSliceSegmentInPicFlag) return PICTURE_TYPE_UNKNOWN;
+                    if (nalUnitType >= BLA_W_LP && nalUnitType <= RSV_IRAP_VCL23) {
+                        bitArray.readBit();  // no_output_of_prior_pics_flag
+                    }
+                    bitArray.readUEV(); // slice_pic_parameter_set_id
+                    // FIXME: Assumes num_extra_slice_header_bits element of PPS data to be 0
+                    int sliceType = bitArray.readUEV();
+                    if (sliceType == 0) {
+                        return PICTURE_TYPE_B;
+                    } else if (sliceType == 1) {
+                        return PICTURE_TYPE_P;
+                    } else if (sliceType == 2) {
+                        return PICTURE_TYPE_I;
+                    } else {
+                        return PICTURE_TYPE_UNKNOWN;
+                    }
+                }
+                pos = offset;
+            }
             return PICTURE_TYPE_UNKNOWN;
         }
     }
 
-    public static int getFrameTypeFromBitStream(String mediaType, ByteBuffer buf,
-            MediaCodec.BufferInfo info) {
-        int frameType = PICTURE_TYPE_UNKNOWN;
-        boolean isAvc = mediaType.equals(MediaFormat.MIMETYPE_VIDEO_AVC);
-        boolean isHevc = mediaType.equals(MediaFormat.MIMETYPE_VIDEO_HEVC);
-        if (!(isAvc || isHevc)) return frameType;
+    public static ParserBase getParserObject(String mediaType) {
+        switch (mediaType) {
+            case MediaFormat.MIMETYPE_VIDEO_AVC:
+                return new AvcParser();
+            case MediaFormat.MIMETYPE_VIDEO_HEVC:
+                return new HevcParser();
+        }
+        return null;
+    }
+
+    public static int getFrameTypeFromBitStream(ByteBuffer buf, MediaCodec.BufferInfo info,
+            ParserBase o) {
         byte[] dataArray = new byte[info.size];
         buf.position(info.offset);
         buf.get(dataArray);
-        for (int pos = 0; pos + 3 < info.size; ) {
-            int startOffset = BitStreamUtils.getNalUnitStartOffset(dataArray, pos, info.size);
-            if (startOffset != NO_NAL_UNIT_FOUND) {
-                int nalUnitType;
-                if (isAvc) {
-                    nalUnitType = getAvcNalUnitType(dataArray, (pos + startOffset));
-                    if (nalUnitType == 1 || nalUnitType == 2 || nalUnitType == 5) {
-                        frameType = parseAvcCodecSliceNalUnit(dataArray, (pos + startOffset),
-                                info.size);
-                        break;
-                    }
-                } else {
-                    nalUnitType = getHevcNalUnitType(dataArray, (pos + startOffset));
-                    if ((nalUnitType >= TRAIL_N && nalUnitType <= RASL_R)
-                            || (nalUnitType >= BLA_W_LP && nalUnitType <= RSV_IRAP_VCL23)) {
-                        frameType = parseHevcSliceSegmentNalUnit(dataArray, (pos + startOffset),
-                                info.size, nalUnitType);
-                        break;
-                    }
-                }
-                pos += startOffset;
-            } else {
-                pos++;
-            }
-        }
-        return frameType;
+        o.set(dataArray, 0, info.size);
+        return o.getFrameType();
     }
 }
