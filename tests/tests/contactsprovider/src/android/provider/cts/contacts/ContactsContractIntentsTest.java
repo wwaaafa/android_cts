eaae1484ba2afe6f6bca59572355c5219d623267
Tingting Wang <tingtingw@google.com>
2023-02-22 14:44:58 -0800

    Skip setDefaultAccount CTS test on watch.
    Test: presubmit
    Bug: 251078355
    Change-Id: I730498205df9159fb99252c45befeef9e83d5b26

--- tests/tests/contactsprovider/src/android/provider/cts/contacts/ContactsContractIntentsTest.java
+++ tests/tests/contactsprovider/src/android/provider/cts/contacts/ContactsContractIntentsTest.java
@@ -56,8 +56,12 @@ public class ContactsContractIntentsTest extends AndroidTestCase {
     }
 
     public void testSetDefaultAccount() {
-        Intent intent = new Intent(ContactsContract.Settings.ACTION_SET_DEFAULT_ACCOUNT);
         PackageManager packageManager = getContext().getPackageManager();
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_WATCH)) {
+            return; // Skip test on watch since the intent is not required.
+        }
+
+        Intent intent = new Intent(ContactsContract.Settings.ACTION_SET_DEFAULT_ACCOUNT);
         List<ResolveInfo> resolveInfoList = packageManager.queryIntentActivities(intent, 0);
         assertNotNull("Missing ResolveInfo", resolveInfoList);
         int handlerCount = 0;
