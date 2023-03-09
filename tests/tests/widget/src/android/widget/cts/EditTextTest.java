230e36b58848669754fb61d2bf4ce8419fa1539f
Pooja Singh <quic_spooja@quicinc.com>
2023-03-09 20:36:03 +0530

    Fix isWatch() to identify correct uimode.
    Bug: 269974149
    Test: Run android.widget.cts.EditTextTest#testHyphenationFrequencyDefaultValueto make sure testcase works fine for all type of UI mode.
    (cherry picked from https://android-review.googlesource.com/q/commit:c540b34b404c5c0a9f7aab8a3bf4afcfd9d17572)
    Merged-In: I7c6734ac592954dd6c30f30801909496bb59c428
    Change-Id: I7c6734ac592954dd6c30f30801909496bb59c428

--- tests/tests/widget/src/android/widget/cts/EditTextTest.java
+++ tests/tests/widget/src/android/widget/cts/EditTextTest.java
@@ -363,7 +363,7 @@ public class EditTextTest {
 
     private boolean isWatch() {
         return (mActivity.getResources().getConfiguration().uiMode
-                & Configuration.UI_MODE_TYPE_WATCH) == Configuration.UI_MODE_TYPE_WATCH;
+                & Configuration.UI_MODE_TYPE_MASK) == Configuration.UI_MODE_TYPE_WATCH;
     }
 
     @Test
