41db907e8ccb8b6164bc7d51ce65534f936e93fc
Felipe Leme <felipeal@google.com>
2023-04-21 15:17:05 -0700

    Disabled A11Y-related tests for visible bg users.
    Test: atest --user-type secondary_user_on_secondary_display ToastTest#testAccessDuration_whenTextToastAndWithA11yTimeoutEnabled,testAccessDuration_whenCustomToastAndWithA11yTimeoutEnabled
    Test: atest ToastTest
    
    Bug: 255426725
    Bug: 272376728
    
    Change-Id: I988ac9dbdb5fad6eb3e69bbdc037841f2aad11c7

--- tests/tests/widget/src/android/widget/cts/ToastTest.java
+++ tests/tests/widget/src/android/widget/cts/ToastTest.java
@@ -66,6 +66,8 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import com.android.bedstead.harrier.DeviceState;
+import com.android.bedstead.harrier.annotations.RequireRunNotOnVisibleBackgroundNonProfileUser;
 import com.android.compatibility.common.util.ApiTest;
 import com.android.compatibility.common.util.PollingCheck;
 import com.android.compatibility.common.util.SystemUtil;
@@ -75,6 +77,7 @@ import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -124,6 +127,12 @@ public class ToastTest {
     public ActivityTestRule<CtsActivity> mActivityRule =
             new ActivityTestRule<>(CtsActivity.class);
     private UiAutomation mUiAutomation;
+
+    // TODO(b/255426725): only used due on @RequireRunNotOnVisibleBackgroundNonProfileUser tests -
+    // remove it - and the Harrier dependency on Android.bp - when A11Y supports it
+    @ClassRule
+    @Rule
+    public static final DeviceState sDeviceState = new DeviceState();
 
     @Before
     public void setup() {
@@ -470,6 +479,8 @@ public class ToastTest {
         assertTrue(longDuration > shortDuration);
     }
 
+    // TODO(b/255426725): remove @Require... when A11Y supports it
+    @RequireRunNotOnVisibleBackgroundNonProfileUser
     @Test
     public void testAccessDuration_whenCustomToastAndWithA11yTimeoutEnabled() throws Throwable {
         makeCustomToast();
@@ -501,6 +512,8 @@ public class ToastTest {
         }
     }
 
+    // TODO(b/255426725): remove @Require... when A11Y supports it
+    @RequireRunNotOnVisibleBackgroundNonProfileUser
     @Test
     public void testAccessDuration_whenTextToastAndWithA11yTimeoutEnabled() throws Throwable {
         makeTextToast();
