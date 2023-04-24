b59cb1a15854a473d5cd670a6c3b231cdb70a3c5
Hiroki Sato <hirokisato@google.com>
2023-04-24 11:28:33 +0900

    Add visibility check in CtsWindowInfoUtils
    Now that WindowInfosListenerForTest exposes invisible windows with a
    flag. This adjusts tests so that existing tests won't be affected.
    
    Bug: 226803477
    Test: CtsWindowManagerDeviceTestCases:SurfaceControlViewHostTests CtsWindowManagerDeviceTestCases:WindowInputTests
    Change-Id: I856126a4f5af0cae33f9f67f52ab21ba4113dc08

--- tests/framework/base/windowmanager/util/src/android/server/wm/CtsWindowInfoUtils.java
+++ tests/framework/base/windowmanager/util/src/android/server/wm/CtsWindowInfoUtils.java
@@ -91,7 +91,7 @@ public class CtsWindowInfoUtils {
     }
 
     /**
-     * Calls the provided predicate each time window information changes if a
+     * Calls the provided predicate each time window information changes if a visible
      * window is found that matches the supplied window token.
      *
      * <p>
@@ -120,6 +120,9 @@ public class CtsWindowInfoUtils {
             }
 
             for (var windowInfo : windowInfos) {
+                if (!windowInfo.isVisible) {
+                    continue;
+                }
                 if (windowInfo.windowToken == windowToken) {
                     return predicate.test(windowInfo);
                 }
@@ -209,7 +212,7 @@ public class CtsWindowInfoUtils {
                         targetWindowInfo = windowInfo;
                         break;
                     }
-                    if (windowInfo.isTrustedOverlay) {
+                    if (windowInfo.isTrustedOverlay || !windowInfo.isVisible) {
                         continue;
                     }
                     aboveWindowInfos.add(windowInfo);
