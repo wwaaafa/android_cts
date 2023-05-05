7aeec46ac3ed96bbe96c89e764b42f4fcdac56b5
Weilin Xu <xuweilin@google.com>
2023-05-05 17:04:34 +0000

    Add user existence assumption in dynamic audio CTS
    Added assumption on whether there exists a user assigned to any
    non-primary audio zones for dynamic audio zone CTS.
    
    Bug: 280872561
    Test: atest android.car.cts.CarAudioManagerTest#switchAudioZoneToConfig
    Change-Id: I1d6b43cf06271c263c6628d83574ac3572e6dbd6

--- tests/tests/car/src/android/car/cts/CarAudioManagerTest.java
+++ tests/tests/car/src/android/car/cts/CarAudioManagerTest.java
@@ -749,7 +749,7 @@ public final class CarAudioManagerTest extends AbstractCarTestCase {
         assumeSecondaryZoneConfigs();
         CarAudioZoneConfigInfo zoneConfigInfoSaved =
                 mCarAudioManager.getCurrentAudioZoneConfigInfo(mZoneId);
-        CarAudioZoneConfigInfo zoneConfigInfoSwitchedTo = assumeDifferentZoneConfig(mZoneId);
+        CarAudioZoneConfigInfo zoneConfigInfoSwitchedTo = getNonCurrentZoneConfig(mZoneId);
         Executor callbackExecutor = Executors.newFixedThreadPool(1);
         TestSwitchAudioZoneConfigCallback callback = new TestSwitchAudioZoneConfigCallback();
 
@@ -789,7 +789,7 @@ public final class CarAudioManagerTest extends AbstractCarTestCase {
     public void switchAudioZoneToConfig_withNullExecutor_fails() throws Exception {
         assumeDynamicRoutingIsEnabled();
         assumeSecondaryZoneConfigs();
-        CarAudioZoneConfigInfo zoneConfigInfoSwitchedTo = assumeDifferentZoneConfig(mZoneId);
+        CarAudioZoneConfigInfo zoneConfigInfoSwitchedTo = getNonCurrentZoneConfig(mZoneId);
         TestSwitchAudioZoneConfigCallback callback = new TestSwitchAudioZoneConfigCallback();
 
         NullPointerException exception = assertThrows(NullPointerException.class,
@@ -806,7 +806,7 @@ public final class CarAudioManagerTest extends AbstractCarTestCase {
     public void switchAudioZoneToConfig_withNullCallback_fails() throws Exception {
         assumeDynamicRoutingIsEnabled();
         assumeSecondaryZoneConfigs();
-        CarAudioZoneConfigInfo zoneConfigInfoSwitchedTo = assumeDifferentZoneConfig(mZoneId);
+        CarAudioZoneConfigInfo zoneConfigInfoSwitchedTo = getNonCurrentZoneConfig(mZoneId);
         Executor callbackExecutor = Executors.newFixedThreadPool(1);
 
         NullPointerException exception = assertThrows(NullPointerException.class,
@@ -991,17 +991,20 @@ public final class CarAudioManagerTest extends AbstractCarTestCase {
     }
 
     private List<TestZoneConfigInfo> assumeSecondaryZoneConfigs() {
+        List<Integer> audioZonesWithPassengers = getAvailablePassengerAudioZone();
+        assumeFalse("Requires a zone with a passenger/user", audioZonesWithPassengers.isEmpty());
         SparseArray<List<TestZoneConfigInfo>> zoneConfigs = parseAudioZoneConfigs();
         List<TestZoneConfigInfo> secondaryZoneConfigs = null;
-        for (int index = 0; index < zoneConfigs.size(); index++) {
-            int zoneId = zoneConfigs.keyAt(index);
-            if (zoneId != INVALID_AUDIO_ZONE && zoneId != PRIMARY_AUDIO_ZONE) {
+        for (int index = 0; index < audioZonesWithPassengers.size(); index++) {
+            int zoneId = audioZonesWithPassengers.get(index);
+            if (zoneConfigs.contains(zoneId) && zoneConfigs.get(zoneId).size() > 1) {
                 mZoneId = zoneId;
-                secondaryZoneConfigs = zoneConfigs.valueAt(index);
+                secondaryZoneConfigs = zoneConfigs.get(zoneId);
                 break;
             }
         }
-        assumeTrue("Secondary zone exists", secondaryZoneConfigs != null);
+        assumeTrue("Secondary zones requires multiple zone configurations",
+                secondaryZoneConfigs != null);
         return secondaryZoneConfigs;
     }
 
@@ -1020,7 +1023,6 @@ public final class CarAudioManagerTest extends AbstractCarTestCase {
             int zoneId = Integer.parseInt(zoneConfigMatcher.group(3));
             int zoneConfigId = Integer.parseInt(zoneConfigMatcher.group(2));
             String configName = zoneConfigMatcher.group(1);
-            boolean isDefault = Boolean.getBoolean(zoneConfigMatcher.group(4));
             if (!zoneConfigs.contains(zoneId)) {
                 zoneConfigs.put(zoneId, new ArrayList<>());
             }
@@ -1029,7 +1031,7 @@ public final class CarAudioManagerTest extends AbstractCarTestCase {
         return zoneConfigs;
     }
 
-    private CarAudioZoneConfigInfo assumeDifferentZoneConfig(int zoneId) {
+    private CarAudioZoneConfigInfo getNonCurrentZoneConfig(int zoneId) {
         List<CarAudioZoneConfigInfo> zoneConfigInfos =
                 mCarAudioManager.getAudioZoneConfigInfos(zoneId);
         CarAudioZoneConfigInfo currentZoneConfigInfo =
@@ -1043,7 +1045,6 @@ public final class CarAudioManagerTest extends AbstractCarTestCase {
             }
         }
 
-        assumeTrue("Different zone configuration exists", differentZoneConfig != null);
         return differentZoneConfig;
     }
 
