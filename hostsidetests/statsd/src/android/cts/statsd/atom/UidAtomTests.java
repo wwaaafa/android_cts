177d9626e5c09d81005b800e600831cc7e7ab01a
maheshkkv <maheshkkv@google.com>
2023-07-13 20:48:01 -0700

    [DO NOT MERGE] Fix WifiLock CTS test cases
    For low latency lock to be active, wifi needs to be connected, screen
    should be ON and application should be in foreground. Make sure the
    pre-requisites are met before running low latency tests.
    
    MUST_SLEEP
    
    Bug: 266749003
    Bug: 286810436
    Test: atest
    CtsStatsdAtomHostTestCases:android.cts.statsd.atom.UidAtomTests#testWifiLockLowLatency
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:3328d093f26f72cfc8768e4aaf8d6ad507ace59a)
    Merged-In: I7ba14bb324f19b3be6c6587c938a9543deb87310
    Change-Id: I7ba14bb324f19b3be6c6587c938a9543deb87310

--- hostsidetests/statsd/src/android/cts/statsd/atom/UidAtomTests.java
+++ hostsidetests/statsd/src/android/cts/statsd/atom/UidAtomTests.java
@@ -1004,7 +1004,18 @@ public class UidAtomTests extends DeviceAtomTestCase {
         List<Set<Integer>> stateSet = Arrays.asList(lockOn, lockOff);
 
         createAndUploadConfig(atomTag, true);  // True: uses attribution.
-        runDeviceTests(DEVICE_SIDE_TEST_PACKAGE, ".AtomTests", "testWifiLockLowLatency");
+
+        // For low latency lock to be active following conditions need to be met,
+        //    - Wi-Fi is connected
+        //    - Screen On
+        //    - Application is foreground.
+
+        // Turn screen on.
+        turnScreenOn();
+
+        // Acquire and release low latency lock in foreground activity.
+        runActivity("StatsdCtsForegroundActivity", "action", "action.acquire_release_wifi_ll_lock");
+        Thread.sleep(WAIT_TIME_SHORT);
 
         // Sorted list of events in order in which they occurred.
         List<EventMetricData> data = getEventMetricDataList();
