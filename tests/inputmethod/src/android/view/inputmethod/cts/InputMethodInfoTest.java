fb2201506bdba01a826b9b082b0e876fad2ddcc7
Eric Rahm <erahm@google.com>
2023-07-27 08:00:41 -0700

    Disable input encryption test on Wear
    `testAtLeastOneEncryptionAwareInputMethodIsAvailable` is no longer a
    valid test on Wear now that a gboard dependency has been removed at boot
    time has been removed. This CL adds a check that target is not a watch
    before running the test.
    
    Fixes: 292042580
    Test: atest CtsInputMethodTestCases:android.view.inputmethod.cts.InputMethodInfoTest#testAtLeastOneEncryptionAwareInputMethodIsAvailable
    Change-Id: Ia8118cf3f229451dfc8f05fece6224104a5647fb
    Merged-In: Ia8118cf3f229451dfc8f05fece6224104a5647fb

--- tests/inputmethod/src/android/view/inputmethod/cts/InputMethodInfoTest.java
+++ tests/inputmethod/src/android/view/inputmethod/cts/InputMethodInfoTest.java
@@ -19,6 +19,7 @@ package android.view.inputmethod.cts;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeFalse;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
@@ -43,6 +44,9 @@ import android.view.inputmethod.InputMethodSubtype;
 import androidx.test.filters.SmallTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
+
+import com.android.compatibility.common.util.FeatureUtil;
+import com.android.compatibility.common.util.PropertyUtil;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -226,6 +230,8 @@ public class InputMethodInfoTest {
 
     @Test
     public void testAtLeastOneEncryptionAwareInputMethodIsAvailable() {
+        assumeFalse(FeatureUtil.isWatch());
+
         if (!mContext.getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_INPUT_METHODS)) {
             return;
